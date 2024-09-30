ae2961f40547756f1b9509e07ccaf1899754af4f
Szers <szersszers@gmail.com>
2024-09-30 16:15:43 +0700

    [FiRST]

---
+++ src/main/java/tdc/edu/vn/project_mobile_be/ProjectMobileBeApplication.java
@@ -0,0 +1,13 @@
+package tdc.edu.vn.project_mobile_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMobileBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMobileBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/tdc/edu/vn/project_mobile_be/services/impl/ProductService.java
@@ -0,0 +1,4 @@
+package tdc.edu.vn.project_mobile_be.services.impl;
+
+public class ProductService {
+}
