2d5a6a62c631df654b946a41a4d8263b917d1bdd
Szers <szersszers@gmail.com>
2024-10-26 21:24:01 +0700

    [none]

--- src/main/java/tdc/edu/vn/project_mobile_be/interfaces/reponsitory/SizeProductRepository.java
+++ src/main/java/tdc/edu/vn/project_mobile_be/interfaces/reponsitory/SizeProductRepository.java
@@ -1,11 +1,10 @@
-//package tdc.edu.vn.project_mobile_be.interfaces.reponsitory;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import tdc.edu.vn.project_mobile_be.entities.relationship.SizeProductId;
-//
-//public interface SizeProductRepository extends JpaRepository<SizeProductRepository, SizeProductId> {
-//
-//
-//
-//}
+package tdc.edu.vn.project_mobile_be.interfaces.reponsitory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+import tdc.edu.vn.project_mobile_be.entities.relationship.SizeProduct;
+import tdc.edu.vn.project_mobile_be.entities.relationship.SizeProductId;
+@Repository
+public interface SizeProductRepository extends JpaRepository<SizeProduct, SizeProductId> {
+}
