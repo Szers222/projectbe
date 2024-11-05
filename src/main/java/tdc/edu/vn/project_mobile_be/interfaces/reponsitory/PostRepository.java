c0bc51583556c220fe5b407e6e5f8b25958300a7
Szers <szersszers@gmail.com>
2024-11-05 17:58:15 +0700

    [DONE]

--- src/main/java/tdc/edu/vn/project_mobile_be/interfaces/reponsitory/PostRepository.java
+++ src/main/java/tdc/edu/vn/project_mobile_be/interfaces/reponsitory/PostRepository.java
@@ -1,5 +1,7 @@
 package tdc.edu.vn.project_mobile_be.interfaces.reponsitory;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,14 +16,18 @@ import java.util.UUID;
 public interface PostRepository extends JpaRepository<Post, UUID> {
     Optional<Post> findByPostId(UUID postId);
 
-    List<Post> findAll();
-
     @Query("SELECT p FROM Post p ORDER BY p.createdAt DESC LIMIT 10")
-
     List<Post> findAllByOrderByCreatedAtDesc();
 
     @Query("select p from Post p left join fetch p.product pr where pr.productId = :productId")
     Optional<Post> findPostByProductId(@Param("productId") UUID productId);
 
+    @Query("SELECT p FROM Post p left join fetch p.postStatus ps WHERE ps.postStatusType = :postStatusType")
+    Page<Post> findAllByPostStatusType(@Param("postStatusType") int postStatusType, Pageable pageable);
 
+    @Query("SELECT p FROM Post p left join fetch p.user u WHERE u.userId = :userId")
+    List<Post> findAllByUserId(@Param("userId") UUID userId);
+
+    @Query("SELECT p FROM Post p where p.postName like %:postName%")
+    List<Post> findPostByName(@Param("postName") String postName);
 }
