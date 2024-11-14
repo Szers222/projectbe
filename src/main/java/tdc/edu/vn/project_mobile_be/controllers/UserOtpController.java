dbe8c31bda1609c22939bc1f289e8eb556dfc901
Szers <szersszers@gmail.com>
2024-11-15 00:36:02 +0700

    [pending]

--- src/main/java/tdc/edu/vn/project_mobile_be/controllers/UserOtpController.java
+++ src/main/java/tdc/edu/vn/project_mobile_be/controllers/UserOtpController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import tdc.edu.vn.project_mobile_be.commond.ResponseData;
 import tdc.edu.vn.project_mobile_be.dtos.requests.EmailRequestDTO;
