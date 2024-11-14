5c07a603da34aa5062aa189f044dc0d1a94a955b
Szers <szersszers@gmail.com>
2024-11-14 22:35:30 +0700

    [none]

--- src/main/java/tdc/edu/vn/project_mobile_be/interfaces/service/OrderService.java
+++ src/main/java/tdc/edu/vn/project_mobile_be/interfaces/service/OrderService.java
@@ -2,6 +2,7 @@ package tdc.edu.vn.project_mobile_be.interfaces.service;
 
 import tdc.edu.vn.project_mobile_be.dtos.requests.order.OrderChangeStatusDTO;
 import tdc.edu.vn.project_mobile_be.dtos.requests.order.OrderCreateRequestDTO;
+import tdc.edu.vn.project_mobile_be.dtos.responses.cart.CartResponseDTO;
 import tdc.edu.vn.project_mobile_be.dtos.responses.order.OrderResponseDTO;
 import tdc.edu.vn.project_mobile_be.entities.order.Order;
 import tdc.edu.vn.project_mobile_be.interfaces.IService;
@@ -17,4 +18,7 @@ public interface OrderService extends IService<Order, UUID> {
 
 
     List<OrderResponseDTO> getOrderByUserId(UUID userId);
+
+    List<CartResponseDTO> getCartByOrderId(UUID orderId);
+
 }
