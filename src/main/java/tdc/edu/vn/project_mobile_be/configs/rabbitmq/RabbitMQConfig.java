4e623d1f0e4310f5b6116cdc23d6359bd739f989
Szers <szersszers@gmail.com>
2024-11-21 21:13:02 +0700

    [none]

--- src/main/java/tdc/edu/vn/project_mobile_be/configs/rabbitmq/RabbitMQConfig.java
+++ src/main/java/tdc/edu/vn/project_mobile_be/configs/rabbitmq/RabbitMQConfig.java
@@ -5,47 +5,28 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tdc.edu.vn.project_mobile_be.commond.component.Receiver;
 
 @Configuration
 public class RabbitMQConfig {
 
-    static final String topicExchangeName = "product-exchange";
-
-    static final String queueName = "product/updates";
+    public static final String EXCHANGE_NAME = "productLogExchange";
+    public static final String QUEUE_NAME = "productLogQueue";
+    public static final String ROUTING_KEY = "product.log";
 
     @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
     }
 
     @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(topicExchangeName);
+    public Queue queue() {
+        return new Queue(QUEUE_NAME);
     }
 
     @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("product.#");
-    }
-
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-
-        container.setQueueNames(queueName);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
 }
