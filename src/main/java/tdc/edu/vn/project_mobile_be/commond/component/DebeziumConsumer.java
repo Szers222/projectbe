bd2c24f3a541ac60ae35f0e1adb5867319e571aa
Szers <szersszers@gmail.com>
2024-11-22 10:23:29 +0700

    [no]

--- src/main/java/tdc/edu/vn/project_mobile_be/commond/component/DebeziumConsumer.java
+++ src/main/java/tdc/edu/vn/project_mobile_be/commond/component/DebeziumConsumer.java
@@ -1,4 +1,22 @@
 package tdc.edu.vn.project_mobile_be.commond.component;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DebeziumConsumer {
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @KafkaListener(topics = "products", groupId = "product-group")
+    public void listen(ConsumerRecord<String, String> record) {
+        // Xử lý event từ Debezium
+        String event = record.value();
+        System.out.println("Received event: " + event);
+        // Gửi message đến client qua WebSocket
+        this.template.convertAndSend("/topic/products", event);
+    }
 }
